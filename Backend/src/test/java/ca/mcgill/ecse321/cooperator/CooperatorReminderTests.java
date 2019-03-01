92e7d8f80c71f2869b890d2ceb6ce742ab3d1d39
JulienLesaffre <julien.lesaffre@mail.mcgill.ca>
2019-03-01 15:28:08 -0500

    .

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/CooperatorReminderTests.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/CooperatorReminderTests.java
@@ -94,37 +94,38 @@ public class CooperatorReminderTests {
 	public void testReminderQueryNotFound() {
 		assertNull(service.getReminder(INVALID_KEY));
 	}
-//	@Test
-//	public void testRestService() {
-//		try {
-//
-//			//Send Reminders to problematic students
-//			URL url = new URL(
-//					"http://cooperator-backend-3417.herokuapp.com/reminders/send");
-//			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//			conn.setRequestMethod("GET");
-//			conn.setRequestProperty("Accept", "application/json");
-//			if (conn.getResponseCode() != 200) {
-//				throw new RuntimeException("Failed : HTTP error code : "
-//						+ conn.getResponseCode());
-//			}		
-//			BufferedReader br = new BufferedReader(new InputStreamReader(
-//					(conn.getInputStream())));
-//			String output;
-//			System.out.println("Output from Server .... \n");
-//			while ((output = br.readLine()) != null) {
-//				System.out.println(output);
-//			}
-//			conn.disconnect();
-//
-//		} catch (MalformedURLException e) {
-//
-//			e.printStackTrace();
-//
-//		} catch (IOException e) {
-//
-//			e.printStackTrace();
-//
-//		}
-//	}
+	@Test
+	public void testRestService() {
+		try {
+
+			//Send Reminders to problematic students
+			URL url = new URL(
+					"http://cooperator-backend-3417.herokuapp.com/reminders/send");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}		
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			assertEquals(200, conn.getResponseCode());
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	}
 }
