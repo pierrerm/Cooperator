28c83c5b0386921b12311d7fb713880b3e75673a
JulienLesaffre <julien.lesaffre@mail.mcgill.ca>
2019-03-01 16:43:28 -0500

    .

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestCooperatorRESTServices.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestCooperatorRESTServices.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.cooperator.service;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -35,5 +36,40 @@ public class TestCooperatorRESTServices {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	@Test
+	public void testSendReminders() {
+		try {
+
+			//Send Reminders to problematic students
+			URL url = new URL(
+					"http://cooperator-backend-3417.herokuapp.com/reminders/send");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}		
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+			assertEquals(200, conn.getResponseCode());
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
 	}
 }
