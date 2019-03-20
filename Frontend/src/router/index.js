10317a699d756032a70b30f2435b61e3f360760d
Anudruth <anudruth@gmail.com>
2019-03-20 15:11:10 -0400

    removed group 19 tests which were causing errors

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import EventRegistration from '@/components/Cooperator'
+import Cooperator from '@/components/Cooperator'
 
 Vue.use(Router)
 
@@ -14,8 +14,8 @@ export default new Router({
     },
     {
       path: '/app',
-      name: 'EventRegistration',
-      component: EventRegistration
+      name: 'Cooperator',
+      component: Cooperator
     }
   ]
 })
