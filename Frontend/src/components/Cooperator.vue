2d95930105980db74f565c37d17f8f1bc83a539a
Matteo Nunez <matteo.nunez22@gmail.com>
2019-03-22 16:20:05 -0400

    Tested calling backend services

--- Frontend/src/components/Cooperator.vue
+++ Frontend/src/components/Cooperator.vue
@@ -2,30 +2,39 @@
   <div id="cooperator">
     <h2>People</h2>
     <table>
-      <tr>
-        <td>John</td>
-        <td>Event to attend</td>
+      <tr v-for="person in people" >
+        <td>{{ person.name }}</td>
+        <td>
+          <ul>
+            <li v-for="event in person.events">
+              {{event.name}}
+            </li>
+          </ul>
+        </td>
       </tr>
       <tr>
         <td>
-          <input type="text" placeholder="Person Name">
+          <input type="text" v-model="newPerson" placeholder="Person Name">
         </td>
         <td>
-          <button>Create</button>
+          <button @click="createPerson(newPerson)">Create Person</button>
+        </td>
+        <td>
+          <button @click="getPerson()">Get Person</button>
         </td>
       </tr>
     </table>
     <p>
-      <span style="color:red">Error: Message text comes here</span>
+      <span v-if="errorPerson" style="color:red">Error: {{errorPerson}}</span>
     </p>
   </div>
 </template>
 
-<script>
+<script src="./administration.js">
 </script>
 
 <style>
-  #eventregistration {
+  #cooperator {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     color: #2c3e50;
     background: #f2ece8;
--- Frontend/src/components/administration.js
+++ Frontend/src/components/administration.js
@@ -2,14 +2,79 @@ import axios from 'axios'
 var config = require('../../config')
 
 var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var backendUrl = 'https://cooperator-backend-3417.herokuapp.com'
 
 var AXIOS = axios.create({
 	baseURL: backendUrl,
-	headers: { 'Access-Control-Allow-Origin': frontendUrl }
+	headers: {
+	  'Access-Control-Allow-Origin': frontendUrl
+    //'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT, OPTIONS',
+    //'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept'
+	}
 })
 
+export default {
+  name: 'cooperator',
+  data () {
+    return {
+      people: [],
+      newPerson: '',
+      errorPerson: '',
+      response: []
+    }
+  },
+  created: function () {
+    // Initializing people from backend
+    AXIOS.get(`/students`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.people = response.data
+      })
+      .catch(e => {
+        this.errorPerson = e;
+      });
+  },
+  methods: {
+    createPerson: function (personName) { // Not working
+      AXIOS.post(`/student/`+personName, {}, {})
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.people.push(response.data)
+          this.newPerson = ''
+          this.errorPerson = ''
+        })
+        .catch(e => {
+          var errorMsg = e.message
+          console.log(errorMsg)
+          this.errorPerson = errorMsg
+        });
+    },
 
+    getPerson: function () {
+      AXIOS.get(`/students`)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.people = response.data
+          console.log(response.data[1].firstName)
+          console.log('Get students')
+        })
+        .catch(e => {
+          this.errorPerson = e;
+        });
+    }
+  }
+}
+
+function PersonDto (name) {
+  this.name = name;
+}
+
+function EventDto (name, date, start, end) {
+  this.name = name
+  this.eventDate = date
+  this.startTime = start
+  this.endTime = end
+}
 
 //Constructor menthods
 function AdministratorDto (phone, firstName, lastName, email, password, userId, faculty, id, student){
@@ -137,24 +202,4 @@ function StudentDto(phone, firstName, lastName, email, password, userId, id, aca
 	this.administrator = administrator;
 	this.faculty = faculty;
 	this.coop = coop;
-}
-
-
-created: function () {
-  // Test data
-  const p1 = new PersonDto('John')
-  const p2 = new PersonDto('Jill')
-  // Sample initial content
-  this.people = [p1, p2]
-},
-Add event handling method createPerson():
-
-methods: {
-  createPerson: function (personName) {
-    // Create a new person and add it to the list of people
-    var p = new PersonDto(personName)
-    this.people.push(p)
-    // Reset the name field for new people
-    this.newPerson = ''
-  }
 }
