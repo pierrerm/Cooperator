c2d328892a4f17db55bf2a4fe8124d61ee7eef97
Romain Couperier <romain.couperier@mail.mcgill.ca>
2019-03-23 17:51:02 -0400

    removed group 3 tests

--- Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestGroup03RESTServices.java
+++ Backend/src/test/java/ca/mcgill/ecse321/cooperator/service/TestGroup03RESTServices.java
@@ -30,37 +30,37 @@ public class TestGroup03RESTServices {
 	@Test
 	public void testQueryServices() {
 
-		try {
-			//get all coops
-			URL url1 = new URL(
-					"https://sturegistration-backend-009b01.herokuapp.com/allUsers");
-			HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
-			conn1.setRequestMethod("GET");
-			assertEquals(200, conn1.getResponseCode());
-			conn1.disconnect();
-			
-			//get all courses
-			URL url2 = new URL(
-					"https://sturegistration-backend-009b01.herokuapp.com/allCoopPositions");
-			HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
-			conn2.setRequestMethod("GET");
-			assertEquals(200, conn2.getResponseCode());
-			conn2.disconnect();
-			
-			//get list of ranked courses
-			URL url3 = new URL(
-					"https://sturegistration-backend-009b01.herokuapp.com/allCoopCourses");
-			HttpURLConnection conn3 = (HttpURLConnection) url3.openConnection();
-			conn3.setRequestMethod("GET");
-			assertEquals(200, conn3.getResponseCode());
-			conn3.disconnect();
-
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			//get all coops
+//			URL url1 = new URL(
+//					"https://sturegistration-backend-009b01.herokuapp.com/allUsers");
+//			HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
+//			conn1.setRequestMethod("GET");
+//			assertEquals(200, conn1.getResponseCode());
+//			conn1.disconnect();
+//			
+//			//get all courses
+//			URL url2 = new URL(
+//					"https://sturegistration-backend-009b01.herokuapp.com/allCoopPositions");
+//			HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+//			conn2.setRequestMethod("GET");
+//			assertEquals(200, conn2.getResponseCode());
+//			conn2.disconnect();
+//			
+//			//get list of ranked courses
+//			URL url3 = new URL(
+//					"https://sturegistration-backend-009b01.herokuapp.com/allCoopCourses");
+//			HttpURLConnection conn3 = (HttpURLConnection) url3.openConnection();
+//			conn3.setRequestMethod("GET");
+//			assertEquals(200, conn3.getResponseCode());
+//			conn3.disconnect();
+//
+//			
+//		} catch (MalformedURLException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		
 	}
 
