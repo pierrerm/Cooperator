ade34fb6826b62a5624a366cd0a3afcf863ad4de
Romain Couperier <romain.couperier@mail.mcgill.ca>
2019-03-24 17:10:50 -0400

    testing new file

---
+++ Frontend/src/components/Reminder.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="id">
+    <div class="site-wrap">
+        <div class="site-section" style="padding-top:10%">
+            <div class="container" style="align:center">
+                <h1 class="page-title">All Reminders</h1>
+                <div id="table">
+                    <table class="info-table" style="width: 100%; height: 100%;">
+                        <tr>
+                            <th style="padding:10px">Urgency</th>
+                        </tr>
+                        <tr v-for="reminder in reminders">
+                            <td style="padding:10px">{{ reminder.urgency }}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script src="./javascript/reminder.js">
+</script>
+
+<style>
+</style>
